4cbb4f1dc5d9bd8b70492373bd08e1c03c006c85
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-02 19:12:43 +0200

    Create Model Student Closes #1

---
+++ backend/src/main/java/com/spring/student/model/Student.java
@@ -0,0 +1,26 @@
+package com.spring.student.model;
+
+import com.spring.student.model.enums.Gender;
+
+import java.util.Date;
+
+public class Student {
+
+    private int id;
+
+    private String fullName;
+
+    private Gender gender;
+
+    private String phone;
+
+    private String age;
+
+    private String address;
+
+    private Date dataCreated;
+
+    private String dataUpdated;
+
+
+}
