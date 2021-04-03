485f00acc20ee32dcd5ea31b87db6a3f7de213b4
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-03 14:19:07 +0200

    Adding @CrossOrigin In Repo Closes #18

--- backend/src/main/java/com/spring/student/repo/StudentRepo.java
+++ backend/src/main/java/com/spring/student/repo/StudentRepo.java
@@ -3,7 +3,10 @@ package com.spring.student.repo;
 import com.spring.student.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//@RepositoryRestResource(collectionResourceRel = "stud")
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource
 public interface StudentRepo extends JpaRepository<Student, Long> {
 }
