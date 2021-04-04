ab8801c08818ed07a2711b3b6ece819dee536ce5
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-04 23:50:12 +0200

    Disabled Method(Get, Post, Put, Delete) Closes #32

--- backend/src/main/java/com/spring/student/config/ConfigureRepositoryRestConfiguration.java
+++ backend/src/main/java/com/spring/student/config/ConfigureRepositoryRestConfiguration.java
@@ -1,9 +1,11 @@
 package com.spring.student.config;
 
+import com.spring.student.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
@@ -23,10 +25,22 @@ public class ConfigureRepositoryRestConfiguration implements RepositoryRestConfi
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+
+        HttpMethod[] unsupported = {HttpMethod.GET,HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+        // Disable Http Methods For Student: POST, PUT, DELETE
+        disableHttpMethods(Student.class, config, unsupported);
+
+
         exposeIds(config);
     }
+    public void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] unsupportedMethod) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethod))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethod));
+    }
 
-    private void exposeIds(RepositoryRestConfiguration config) {
+    public void exposeIds(RepositoryRestConfiguration config) {
         //Epose Entity Ids
 
         // Get List Of Entity Classes From The Entity Manaager
--- backend/src/main/java/com/spring/student/repo/StudentRepo.java
+++ backend/src/main/java/com/spring/student/repo/StudentRepo.java
@@ -3,9 +3,11 @@ package com.spring.student.repo;
 import com.spring.student.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@RepositoryRestResource(collectionResourceRel = "stud")
-@RepositoryRestResource
+//@RepositoryRestResource
+@Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
 }
