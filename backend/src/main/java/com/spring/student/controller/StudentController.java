f66d150f57af3938bafe1984bf073c7256b4dc20
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-04 18:17:23 +0200

    Spring Controller(getAllStudents, getStudentById) Closes #28

--- backend/src/main/java/com/spring/student/controller/StudentController.java
+++ backend/src/main/java/com/spring/student/controller/StudentController.java
@@ -1,15 +1,29 @@
 package com.spring.student.controller;
 
 
+import com.spring.student.model.Student;
 import com.spring.student.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class StudentController {
 
     private final StudentService studentService;
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/student")
+    public Student getStudentById(@RequestParam Long id){
+        return studentService.getStudentById(id);
+    }
+
 }
--- backend/src/main/java/com/spring/student/service/StudentService.java
+++ backend/src/main/java/com/spring/student/service/StudentService.java
@@ -1,12 +1,23 @@
 package com.spring.student.service;
 
+import com.spring.student.model.Student;
 import com.spring.student.repo.StudentRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class StudentService {
     
     private final StudentRepo studentRepo;
+
+    public List<Student> getAllStudents(){
+        return studentRepo.findAll();
+    }
+
+    public Student getStudentById(Long id){
+        return studentRepo.findById(id).get();
+    }
 }
