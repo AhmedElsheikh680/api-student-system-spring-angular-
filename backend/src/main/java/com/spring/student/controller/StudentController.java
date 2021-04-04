8cfad0d53e4eee6cba41c006ad8414957a512655
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-04 22:03:06 +0200

    Spring Controller(addStudent) Closes #29

--- backend/src/main/java/com/spring/student/controller/StudentController.java
+++ backend/src/main/java/com/spring/student/controller/StudentController.java
@@ -16,14 +16,22 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    
+    //http://localhost:8080/api/v1/student
     @GetMapping("/students")
     public List<Student> getAllStudents(){
         return studentService.getAllStudents();
     }
 
+    //http://localhost:8080/api/v1/student?id=1
     @GetMapping("/student")
     public Student getStudentById(@RequestParam Long id){
         return studentService.getStudentById(id);
     }
 
+    //http://localhost:8080/api/v1/students
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
 }
--- backend/src/main/java/com/spring/student/service/StudentService.java
+++ backend/src/main/java/com/spring/student/service/StudentService.java
@@ -20,4 +20,8 @@ public class StudentService {
     public Student getStudentById(Long id){
         return studentRepo.findById(id).get();
     }
+
+    public Student addStudent(Student student){
+        return studentRepo.save(student);
+    }
 }
