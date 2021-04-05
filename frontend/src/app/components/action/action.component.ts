5c5c96e31294a35938ecdc58569960619df0eee8
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-05 09:34:22 +0200

    Implmentation formBuilder Of actions(save and update) Components Closes #36

--- frontend/src/app/components/action/action.component.ts
+++ frontend/src/app/components/action/action.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-action',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionComponent implements OnInit {
 
-  constructor() { }
+  studentGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.studentGroup = this.formBuilder.group({
+      student: this.formBuilder.group({
+        fullName: ['fullName'],
+        age: ['age'],
+        address: ['address'],
+        phone: ['phone'],
+        gender: ['gender']
+      })
+    })
   }
 
+  save() {
+
+  }
 }
