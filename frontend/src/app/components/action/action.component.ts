4f6e1ca2fc4dfa08756c12b364b0335b95679a70
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-04-06 16:47:53 +0200

    Impl Validators In html ActionsComponent Closes #48

--- frontend/src/app/components/action/action.component.ts
+++ frontend/src/app/components/action/action.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Student} from '../../model/student';
 import {StudentService} from '../../services/student.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {SpaceValidator} from '../../model/space-validator';
 
 @Component({
   selector: 'app-action',
@@ -34,12 +35,13 @@ export class ActionComponent implements OnInit {
     this.studentGroup = this.formBuilder.group({
       student: this.formBuilder.group({
         fullName: new FormControl('',
-          [Validators.required, Validators.minLength(5)]),
+          [Validators.required, Validators.minLength(5), SpaceValidator.noOnlyWithSpace]),
         age: new FormControl('',
-          [Validators.required, Validators.minLength(2), Validators.pattern("^[0-9]")]),
-        address: new FormControl('', [Validators.required]),
+          [Validators.required, Validators.maxLength(2), Validators.pattern("^[0-9]*$"),
+                          SpaceValidator.noOnlyWithSpace]),
+        address: new FormControl('', [Validators.required,SpaceValidator.noOnlyWithSpace]),
         phone: new FormControl('', [Validators.required,
-          Validators.minLength(11), Validators.pattern("^[0-9]")]),
+          Validators.maxLength(11), Validators.pattern("^[0-9]*$"),SpaceValidator.noOnlyWithSpace]),
         gender: ['MALE']
       })
     })
@@ -60,33 +62,51 @@ export class ActionComponent implements OnInit {
   getGender(){
     return this.studentGroup.get('student').value.gender;
   }
+  get fullName(){
+    return this.studentGroup.get("student.fullName");
+  }
+  get age(){
+    return this.studentGroup.get("student.age");
+  }
+  get address(){
+    return this.studentGroup.get("student.address");
+  }
+  get phone(){
+    return this.studentGroup.get("student.phone");
+  }
+  get gender(){
+    return this.studentGroup.get("student.gender");
+  }
 
   save() {
-    // alert(this.getFullName());
-    // alert(this.getAge());
-    // console.log(`FullName: `+this.getFullName());
-    // console.log(`Age `+this.getAge());
-    // console.log(`Address `+this.getAddress());
-    // console.log(`Phone `+this.getPhone());
-    // console.log(`Gender `+this.getGender());
-    const stu = new Student(this.id, this.getFullName(), this.getGender(), this.getAge(),this.getPhone(),this.getAddress());
-    if(this.id == 0){ //Save
-      this.studentService.addStudent(stu).subscribe(
-        response => {
-          this.router.navigateByUrl('students')
-        },error => {
-          this.invalidFullName='Full Name Aleardy Exist',
-            this.showMessage()
-        }
-      )
-    }else { // Update
-      console.log('updated ' +this.id);
-      this.studentService.editStudent(stu,this.id).subscribe(
-        response => {
-          this.router.navigateByUrl('students')
-        }
-      )
-    }
+
+
+      // alert(this.getFullName());
+      // alert(this.getAge());
+      // console.log(`FullName: `+this.getFullName());
+      // console.log(`Age `+this.getAge());
+      // console.log(`Address `+this.getAddress());
+      // console.log(`Phone `+this.getPhone());
+      // console.log(`Gender `+this.getGender());
+      const stu = new Student(this.id, this.getFullName(), this.getGender(), this.getAge(), this.getPhone(), this.getAddress());
+      if (this.id == 0) { //Save
+        this.studentService.addStudent(stu).subscribe(
+          response => {
+            this.router.navigateByUrl('students')
+          }, error => {
+            this.invalidFullName = 'Full Name Aleardy Exist',
+              this.showMessage()
+          }
+        )
+      } else { // Update
+        console.log('updated ' + this.id);
+        this.studentService.editStudent(stu, this.id).subscribe(
+          response => {
+            this.router.navigateByUrl('students')
+          }
+        )
+      }
+
 
   }
 
